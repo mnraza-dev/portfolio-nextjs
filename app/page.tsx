91aa10e36aca2947ce46f418fe0da8735b9e99e5
mnrazablog <mnraza94@gmail.com>
2024-09-09 04:34:08 +0530

    working on Grid Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
+import Grid from "@/components/Grid";
 import HeroSection from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavigation";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10  px-5 ">
       <div className="max-w-7xl w-full ">
-        <h1 className="p-6 text-6xl font-bold underline">Hello, Next.js</h1>
-
+        <FloatingNav
+          className=""
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: (
+                <FaHome className="h-4 w-4 text-neutral-500 dark:text-white" />
+              ),
+            },
+            {
+              name: "About",
+              link: "/about",
+              icon: (
+                <FaHome className="h-4 w-4 text-neutral-500 dark:text-white" />
+              ),
+            },
+          ]}
+        />
         <HeroSection />
+        <Grid/>
       </div>
-
-      <button>Hello</button>
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,55 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaExternalLinkAlt, FaLocationArrow } from "react-icons/fa";
 
 const HeroSection = () => {
+  const words = `Transfroming Concept Into Seamless User Experiences`;
   return (
     <section className="pb-20 pt-36 ">
       <div className="w-full">
-      <Spotlight className="-top-40  -left-10 md:-left-20 md:-top-20 h-screen" fill="white"/>
-      <Spotlight className="top-10  left-full h-[80vh] w-[50vw" fill="purple"/>
-      <Spotlight className="top-28  left-80 h-[80vh] w-[50vw" fill="blue"/>
+        <Spotlight
+          className="-top-40  -left-10 md:-left-20 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10  left-full h-[80vh] w-[50vw"
+          fill="purple"
+        />
+        <Spotlight className="top-28  left-80 h-[80vh] w-[50vw" fill="blue" />
+      </div>
+      {/* Grid starts here*/}
+      <div className="h-screen w-full dark:bg-black bg-balck-100  dark:bg-grid-white/[0.1] bg-grid-black/[0.1] absolute top-0 left-0 flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      {/* Grid ends here */}
+
+      <div className="flex justify-center relative my-20 z-10  items-center">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center">
+          <h2 className="uppercase text-xs text-blue-100 tracking-widest  max-w-80 ">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words={words}
+          />
+          <p className="text-center md:tracking-wider text-blue-100 mt-2 mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I'm MN Raza a Next JS Developer based in India.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title={"Get Stared"}
+              position={"right"}
+              otherClass={"w-full"}
+              icon={<FaExternalLinkAlt/>}
+            />
+          </a>
+
+
+        </div>
       </div>
     </section>
   );
