a12a2a1cb140adf2c6534dc4885c8a140fb14f10
mnrazablog <mnraza94@gmail.com>
2024-09-09 04:59:36 +0530

    worked on floating navigation

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 import { gridItems } from "@/data";
@@ -6,12 +7,14 @@ const Grid = () => {
   return (
     <section id="about " className="">
       <BentoGrid>
-        {gridItems.map((item) => (
+        {gridItems.map(({id, title, className, description, imgClassName}) => (
           <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            imgClassName={imgClassName}
           />
         ))}
       </BentoGrid>
